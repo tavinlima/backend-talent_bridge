1a0ca36c5a8b20b3733554351f8a7cc3e6dabf31
Gustavo Lima <glima.0913@gmail.com>
2025-02-04 22:08:05 -0300

    feat: gerar criptografia de senha no cadastro de usuário

--- TalentBridge_webAPI/Repositories/UsuarioRepository.cs
+++ TalentBridge_webAPI/Repositories/UsuarioRepository.cs
@@ -56,7 +56,7 @@ namespace talentbridge_webAPI.Repositories
                     {
                         Email = usuario.Email,
                         Nome = usuario.Nome,
-                        Senha = usuario.Senha,
+                        Senha = Criptografia.GerarHash(usuario.Senha),
                         IdContato = novoContato.IdContato,
                         IdEndereco = novoEndereco.IdEndereco
                     };
